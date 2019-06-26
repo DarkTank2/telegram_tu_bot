9cfc604cb7aec0b6131c659c52614995bf1d261d
DarkTank2 <x.s@gmx.at>
2019-06-27 00:42:16 +0200

    adding scheduling mechanism

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const Telegraf = require('telegraf')
+const moment = require('moment')
+var schedule = require('node-schedule')
 require('dotenv').config()
 
 var jobs = []
+var commandlines = []
+var counter = 0
+var clCnt = 0
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => {
@@ -17,7 +22,11 @@ bot.help((ctx) => {
 
 bot.command('addalert', ctx => {
     console.log('[addalert] ' + ctx.message.text)
-    ctx.reply('Added')
+    commandlines.push({
+        from: ctx.message.from,
+        status: 'addalert'
+    })
+    ctx.reply('Please enter the date in a similar way to the current timestamp-format:\n\n' + moment().format())
 })
 
 bot.command('deletealert', ctx => {
@@ -29,4 +38,58 @@ bot.command('list', ctx => {
     console.log('[list] ' + ctx.message.text)
     ctx.reply('Listed')
 })
+
+bot.on('message', ctx => {
+    console.log('[callback] Message received')
+    var commandline = getCommandlineByUser(ctx.message.from)
+    if (commandline === undefined || commandline.length === 0) {
+        ctx.reply('You, Sir, have no command open!')
+        return
+    }
+    commandline = commandline[0]
+    switch (commandline.status) {
+        case 'addalert':
+            console.log('[case] addalert')
+            commandline.date = ctx.message.text
+            ctx.reply('Please enter the course number and or short description')
+            commandline.status = 'date'
+            break
+        case 'date':
+            console.log('[case] date')
+            commandline.course = ctx.message.text
+            var curJob = {
+                ctx: ctx,
+                id: counter,
+                date: commandline.date,
+                course: commandline.course,
+                from: commandline.from
+            }
+            console.log('Job set for job with id ' + curJob.id + ' at ' + curJob.date)
+            curJob.j = schedule.scheduleJob(curJob.date, function (id) {
+                var job = getJobByID(id)
+                console.log('Executing job with id ' + job.id)
+                job.ctx.reply('!Reminder from ' + job.from.first_name + ' ' + job.from.last_name + '!')
+                job.ctx.reply(job.course)
+            }.bind(null, counter))
+            counter++
+            jobs.push(curJob)
+            break
+    }
+})
 bot.launch()
+
+
+function getCommandlineByUser (user) {
+    console.log('[function] getCommandlineByUser')
+    var retArr = []
+    commandlines.forEach(cl => {
+        if (cl.from.id === user.id) retArr.push(cl)
+    })
+    return retArr
+}
+
+function getJobByID (id) {
+    for (var job of jobs) {
+        if (job.id === id) return job
+    }
+}
